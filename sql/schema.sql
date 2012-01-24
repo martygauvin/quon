6b102b803984799a25422902f987a8762f21f106
serialmonkey <serialmonkey@gmail.com>
2012-01-24 04:39:09 +0000

    Commit DB schema

---
+++ sql/schema.sql
@@ -0,0 +1,376 @@
+-- MySQL dump 10.13  Distrib 5.1.58, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: ands
+-- ------------------------------------------------------
+-- Server version	5.1.58-1ubuntu1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `brandings`
+--
+
+DROP TABLE IF EXISTS `brandings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `brandings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` int(11) NOT NULL,
+  `css` int(11) NOT NULL,
+  `survey_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brandings`
+--
+
+LOCK TABLES `brandings` WRITE;
+/*!40000 ALTER TABLE `brandings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `brandings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `configurations`
+--
+
+DROP TABLE IF EXISTS `configurations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `configurations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `configurations`
+--
+
+LOCK TABLES `configurations` WRITE;
+/*!40000 ALTER TABLE `configurations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `configurations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (3,'Distributed Computing Research Group'),(4,'New Research Group');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `participants`
+--
+
+DROP TABLE IF EXISTS `participants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `participants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_id` int(11) NOT NULL,
+  `given_name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `participants`
+--
+
+LOCK TABLES `participants` WRITE;
+/*!40000 ALTER TABLE `participants` DISABLE KEYS */;
+INSERT INTO `participants` VALUES (1,3,'Mark','Wallis','1992-09-17','','f1d1f840dc911e3e7030808342c00960fbbc8f7b','mark@nsavant.com.au'),(2,3,'David','Paul','2020-01-24','david','8267bf619f2f204fd28bf57aadfba951c3a4a725','david.paul@newcastle.edu.au');
+/*!40000 ALTER TABLE `participants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `survey_instance_object`
+--
+
+DROP TABLE IF EXISTS `survey_instance_object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `survey_instance_object` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_instance_id` int(11) NOT NULL,
+  `survey_object_id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey_instance_object`
+--
+
+LOCK TABLES `survey_instance_object` WRITE;
+/*!40000 ALTER TABLE `survey_instance_object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `survey_instance_object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `survey_instances`
+--
+
+DROP TABLE IF EXISTS `survey_instances`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `survey_instances` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `locked` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey_instances`
+--
+
+LOCK TABLES `survey_instances` WRITE;
+/*!40000 ALTER TABLE `survey_instances` DISABLE KEYS */;
+INSERT INTO `survey_instances` VALUES (1,3,'1.0',0),(2,4,'1.0',0);
+/*!40000 ALTER TABLE `survey_instances` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `survey_object_attributes`
+--
+
+DROP TABLE IF EXISTS `survey_object_attributes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `survey_object_attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_object_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `value` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey_object_attributes`
+--
+
+LOCK TABLES `survey_object_attributes` WRITE;
+/*!40000 ALTER TABLE `survey_object_attributes` DISABLE KEYS */;
+INSERT INTO `survey_object_attributes` VALUES (5,12,'0',''),(6,12,'1','');
+/*!40000 ALTER TABLE `survey_object_attributes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `survey_objects`
+--
+
+DROP TABLE IF EXISTS `survey_objects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `survey_objects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` int(11) NOT NULL,
+  `published` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey_objects`
+--
+
+LOCK TABLES `survey_objects` WRITE;
+/*!40000 ALTER TABLE `survey_objects` DISABLE KEYS */;
+INSERT INTO `survey_objects` VALUES (12,3,'How are you ?',0,0);
+/*!40000 ALTER TABLE `survey_objects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `survey_result_answers`
+--
+
+DROP TABLE IF EXISTS `survey_result_answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `survey_result_answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_result_id` int(11) NOT NULL,
+  `survey_object_instance_id` int(11) NOT NULL,
+  `answer` varchar(200) DEFAULT NULL,
+  `time_spent` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey_result_answers`
+--
+
+LOCK TABLES `survey_result_answers` WRITE;
+/*!40000 ALTER TABLE `survey_result_answers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `survey_result_answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `survey_results`
+--
+
+DROP TABLE IF EXISTS `survey_results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `survey_results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `survey_instance_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `participant_id` int(11) NOT NULL,
+  `test` tinyint(1) NOT NULL,
+  `completed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey_results`
+--
+
+LOCK TABLES `survey_results` WRITE;
+/*!40000 ALTER TABLE `survey_results` DISABLE KEYS */;
+/*!40000 ALTER TABLE `survey_results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `surveys`
+--
+
+DROP TABLE IF EXISTS `surveys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `surveys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `short_name` varchar(20) NOT NULL,
+  `type` int(11) NOT NULL,
+  `multiple_run` tinyint(1) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `live_instance` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `short_name` (`short_name`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `surveys`
+--
+
+LOCK TABLES `surveys` WRITE;
+/*!40000 ALTER TABLE `surveys` DISABLE KEYS */;
+INSERT INTO `surveys` VALUES (3,3,'Marks Survey','mark',1,0,4,NULL),(4,4,'My new survey','newsurvey',0,0,4,NULL);
+/*!40000 ALTER TABLE `surveys` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_groups`
+--
+
+DROP TABLE IF EXISTS `user_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_groups`
+--
+
+LOCK TABLES `user_groups` WRITE;
+/*!40000 ALTER TABLE `user_groups` DISABLE KEYS */;
+INSERT INTO `user_groups` VALUES (7,4,4),(6,4,3),(8,5,4);
+/*!40000 ALTER TABLE `user_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `given_name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `researcher_id` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,1,'researcher','d01f5ed3c2173c7f691eff9294dac1c3d382983f','Mark','Researcher','mark.wallis@uon.edu.au',NULL),(2,0,'admin','64287249e859cccf19672d9c1f808cfbe2a5d28d','System','Administrator','mark@nsavant.com.au',NULL),(5,1,'researcher2','fc422153afa52bee20a48716c4f8d73c8b043bf7','Second','Researcher','mark.wallis@uon.edu.au',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-01-24  4:25:55

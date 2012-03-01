0ed6cc6e18d77e1543366cae6cae72ee58e6bf65
davidjohnpaul@gmail.com <davidjohnpaul@gmail.com@4b731f80-6fd4-f82e-f155-9ce06065d14f>
2012-03-01 00:43:48 +0000

    Added initial support for Likert scale

--- trunk/app/View/Helper/LikertScaleQuestionHelper.php
+++ trunk/app/View/Helper/LikertScaleQuestionHelper.php
@@ -5,23 +5,46 @@ class LikertScaleQuestionHelper extends QuestionHelper {
     
 	protected $attributes = array(0 => array('name' => 'Question Text',
 											 'help' => 'Text to display when asking the user this question'),
-    							  1 => array('name' => 'Left label',
-    							  			 'help' => 'Text to display for the left-most label'),
-    							  2 => array('name' => 'Right label',
-    							  			 'help' => 'Text to display for the right-most label'),
-								  3 => array('name' => 'Number of graduations',
-								  			 'help' => 'Number of options to display between the two labels')
+								  1 => array('name' => 'Options',
+								  			 'help' => 'Options to display for each item, each separated by a |. e.g. For a five-point Likert scale: Strongly disagree|Disagree|Neither agree nor disagree|Agree|Strongly agree'),
+								  2 => array('name' => 'Items',
+		    							  	 'help' => 'Text to display for items, each separated by a |. e.g. Item 1|Item 2'),
 	);
 
 	function renderQuestion($form, $attributes)
 	{
-		// TODO: Render LikeRT scale question type
+		echo "Question: ".$attributes[0]."<br/><br/>";
+		
+		$options = explode("|", $attributes[1]);
+		$items = explode("|", $attributes[2]);
+		
+		// Hack to make empty strings work correctly
+		foreach ($options as $index=>$option)
+		{
+			if ('' == $option)
+			{
+				$options[$index] = '&nbsp;';
+			}
+		}
+		
+		foreach ($items as $index=>$item)
+		{
+			echo $form->input('answer'.$index.'i', array('type'=>'radio', 'legend'=>$item, 'options'=>$options));
+		}
 	}
 	
 	function serialiseAnswer($data, $attributes)
 	{
-		// TODO: Serialise answer for LikeRT question type
-		return;
+		$items = explode("|", $attributes[2]);
+		
+		$results = array();
+		
+		foreach ($items as $index=>$item)
+		{
+			$results[] = $data['Public']['answer'.$index.'i'];
+		}
+		
+		return implode("|", $results);
 	}
 }
 ?>

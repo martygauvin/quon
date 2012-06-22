c99ef5b0af5aa4ad5a200cf885be5bb72dc47973
davidjohnpaul@gmail.com <davidjohnpaul@gmail.com@4b731f80-6fd4-f82e-f155-9ce06065d14f>
2012-06-22 02:13:39 +0000

    Initial support for calculations.

---
+++ trunk/app/View/Helper/CalculationQuestionHelper.php
@@ -0,0 +1,30 @@
+<?php
+App::uses('AppHelper', 'View/Helper');
+
+class CalculationQuestionHelper extends QuestionHelper {	
+	
+	protected $attributes = array(0 => array('name' => 'Calculation', 
+											 'help' => 'Enter the formula for the calculation. Valid operators are: + (addition); - (subtraction); * (multiplication); / (division). Names of questions can be entered in square brackets. e.g. "[mass]/([height]*[height])"'),
+    							  1 => array('name' => 'Display', 
+    							  			 'help' => 'If blank, calculation is not displayed. Otherwise the value from this field is displayed, with "[value]" replaced with the calculated value. e.g. "Your BMI is: [value]"'),
+								  2 => array('name' => 'Error value',
+								  			  'help' => 'The value to use if an error occurs in the calculation. Defaults to "error".')
+	);
+	
+	function renderQuestion($form, $attributes, $previousAnswer, &$show_next)
+	{	
+		$calculatedValue = 0;
+		if ($previousAnswer) {
+			$calculatedValue = $previousAnswer['SurveyResultAnswer']['answer'];
+		}
+		
+		echo str_replace('[value]', $calculatedValue, $attributes[1]);
+		echo $form->hidden('answer', array('value' => $calculatedValue));
+	}
+    
+	function serialiseAnswer($data, $attributes)
+	{
+		return $data['Public']['answer'];
+	}
+}
+?>

71faa75c192cd7b623f5dd78bc8f9cd4baa95d0e
davidjohnpaul@gmail.com <davidjohnpaul@gmail.com@4b731f80-6fd4-f82e-f155-9ce06065d14f>
2012-12-13 05:24:08 +0000

    Allow selection of users to be exported with metadata for a survey.

---
+++ trunk/app/Model/SurveyMetadata.php
@@ -0,0 +1,51 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * SurveyMetadata Model
+ *
+ * @property Survey $Survey
+ * @property SurveyMetadataUser $SurveyMetadataUser
+ */
+class SurveyMetadata extends AppModel {
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Survey' => array(
+			'className' => 'Survey',
+			'foreignKey' => 'survey_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'User' => array(
+			'className' => 'User',
+			'joinTable' => 'survey_metadata_users',
+			'associationForeignKey' => 'user_id',
+			'foreignKey' => 'survey_metadata_id',
+			'unique' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => ''
+		)
+	);
+
+}
